a30713863e71f4a19127f239924999b74b1bab3f
Daniel Centore <daniel.centore@gmail.com>
2017-03-14 12:01:04 -0400

    Fixed dependency hell Made it so the music service projects are of the correct types. Make sure to run ./fetchSubmodules.sh

---
+++ MeNext/Playing/MusicController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using com.danielcentore.MusicService;
+
+namespace MeNext
+{
+	public class MusicController
+	{
+		private List<IMusicService> musicServices;
+
+		public MusicController()
+		{
+			
+		}
+
+
+	}
+}
