666ec67a051969aafa05b79dd7eb01524ef90cbc
ConnorFoody <connorfoody@gmail.com>
2017-03-14 22:51:37 -0700

    added queue ui barebones

---
+++ MeNext/Layout/QueueView.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+
+namespace MeNext
+{
+	public class QueueView : ContentPage
+	{
+		
+		/*
+		private ListView getListView(List<Song> songs)
+		{
+			// Create the ListView.
+			ListView ret = new ListView
+			{
+				// Source of data items.
+				ItemsSource = songs,
+
+				// Define template for displaying each item.
+				// (Argument of DataTemplate constructor is called for 
+				//      each item; it must return a Cell derivative.)
+				ItemTemplate = new DataTemplate(() =>
+					{
+						// Create views with bindings for displaying each property.
+						Label nameLabel = new Label();
+						nameLabel.SetBinding(Label.TextProperty, "Name");
+
+						Label birthdayLabel = new Label();
+						birthdayLabel.SetBinding(Label.TextProperty,
+							new Binding("Birthday", BindingMode.OneWay,
+								null, null, "Born {0:d}"));
+
+						BoxView boxView = new BoxView();
+						boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
+
+						// Return an assembled ViewCell.
+						return new ViewCell
+						{
+							View = new StackLayout
+							{
+								Padding = new Thickness(0, 5),
+								Orientation = StackOrientation.Horizontal,
+								Children =
+								{
+									boxView,
+									new StackLayout
+									{
+										VerticalOptions = LayoutOptions.Center,
+										Spacing = 0,
+										Children =
+										{
+											nameLabel,
+											birthdayLabel
+										}
+										}
+								}
+							}
+						};
+					})
+			};
+
+			return ret;
+		}
+
+*/
+		public QueueView()
+		{
+			this.Title = "Queue Placeholder";
+
+			List<Song> songs = new List<Song>
+			{
+				new Song("A"),
+				new Song("B"),
+			};
+
+			NavigationPage.SetHasNavigationBar(this, false);
+			Content = new StackLayout
+			{
+				Padding = LayoutConsts.DEFAULT_PADDING,
+				Children = {
+					new Label { Text = "queue holder" },
+					new SongList(songs),
+				}
+			};
+		}
+	}
+}
---
+++ MeNext/Layout/SongList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace MeNext
+{
+	// TODO: images? buttons on the sides?
+	public class Song
+	{
+		public Song(string name)
+		{
+			this.Name = name;
+		}
+
+		public Song(string name, string artist)
+		{
+			this.Name = name;
+			this.Artist = artist;
+		}
+
+		// should we provide getters on these?
+		public string Name 
+		{ 
+			get; 
+		}
+
+		public string Artist
+		{
+			get;
+		}
+	};
+
+	// SongList manages presenting a list of songs. 
+	// Down the road we will extend this to include 
+	public class SongList : ListView
+	{
+		public SongList(List<Song> songs)
+		{
+			this.ItemTemplate = ItemTemplate = new DataTemplate(() =>
+					{
+						// Create views with bindings for displaying each property.
+						Label nameLabel = new Label();
+						nameLabel.SetBinding(Label.TextProperty, "Name");
+
+						Label birthdayLabel = new Label();
+						birthdayLabel.SetBinding(Label.TextProperty,
+							new Binding("Birthday", BindingMode.OneWay,
+								null, null, "Born {0:d}"));
+
+						BoxView boxView = new BoxView();
+						boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
+
+						// Return an assembled ViewCell.
+						return new ViewCell
+						{
+							View = new StackLayout
+							{
+								Padding = new Thickness(0, 5),
+								Orientation = StackOrientation.Horizontal,
+								Children =
+								{
+									boxView,
+									new StackLayout
+									{
+										VerticalOptions = LayoutOptions.Center,
+										Spacing = 0,
+										Children =
+										{
+											nameLabel,
+											birthdayLabel
+										}
+										}
+								}
+							}
+						};
+					});
+
+			this.songs = songs;
+
+			// TODO: look into copy / write mechanics messing with this
+			this.ItemsSource = this.songs;
+		}
+
+		public List<Song> songs
+		{
+			// not sure that we need a "get"
+			get;
+			// set should clear all of the existing songs
+			set;
+
+		}
+
+
+	}
+}
