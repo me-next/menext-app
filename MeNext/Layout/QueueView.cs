81d82cc088531a64a17fbddaa304ce3227cae4e0
ConnorFoody <connorfoody@gmail.com>
2017-03-17 15:14:37 -0700

    Cleanup, started to flesh out the SongView

--- MeNext/Layout/QueueView.cs
+++ MeNext/Layout/QueueView.cs
@@ -7,63 +7,6 @@ namespace MeNext
 {
 	public class QueueView : ContentPage
 	{
-		
-		/*
-		private ListView getListView(List<Song> songs)
-		{
-			// Create the ListView.
-			ListView ret = new ListView
-			{
-				// Source of data items.
-				ItemsSource = songs,
-
-				// Define template for displaying each item.
-				// (Argument of DataTemplate constructor is called for 
-				//      each item; it must return a Cell derivative.)
-				ItemTemplate = new DataTemplate(() =>
-					{
-						// Create views with bindings for displaying each property.
-						Label nameLabel = new Label();
-						nameLabel.SetBinding(Label.TextProperty, "Name");
-
-						Label birthdayLabel = new Label();
-						birthdayLabel.SetBinding(Label.TextProperty,
-							new Binding("Birthday", BindingMode.OneWay,
-								null, null, "Born {0:d}"));
-
-						BoxView boxView = new BoxView();
-						boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
-
-						// Return an assembled ViewCell.
-						return new ViewCell
-						{
-							View = new StackLayout
-							{
-								Padding = new Thickness(0, 5),
-								Orientation = StackOrientation.Horizontal,
-								Children =
-								{
-									boxView,
-									new StackLayout
-									{
-										VerticalOptions = LayoutOptions.Center,
-										Spacing = 0,
-										Children =
-										{
-											nameLabel,
-											birthdayLabel
-										}
-										}
-								}
-							}
-						};
-					})
-			};
-
-			return ret;
-		}
-
-*/
 		public QueueView()
 		{
 			this.Title = "Queue Placeholder";
@@ -75,12 +18,18 @@ namespace MeNext
 			};
 
 			NavigationPage.SetHasNavigationBar(this, false);
+			var songList = new SongList(songs);
+			songList.OnSongSelected += (song) =>
+			{
+				System.Diagnostics.Debug.WriteLine("selected song: " + song.Name);
+			};
+
 			Content = new StackLayout
 			{
 				Padding = LayoutConsts.DEFAULT_PADDING,
 				Children = {
 					new Label { Text = "queue holder" },
-					new SongList(songs),
+					songList,
 				}
 			};
 		}
--- MeNext/Layout/SongList.cs
+++ MeNext/Layout/SongList.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using System.Diagnostics;
 
 using Xamarin.Forms;
 using System.Collections.Generic;
 
 namespace MeNext
 {
-	// TODO: images? buttons on the sides?
+	// Song is a POD class for song information
 	public class Song
 	{
 		public Song(string name)
@@ -29,68 +29,125 @@ namespace MeNext
 		{
 			get;
 		}
+
+		public override string ToString()
+		{
+			return string.Format("[Song: Name={0}, Artist={1}]", Name, Artist);
+		}
+	};
+
+	// SongFactory controls how the song gets rendered for the SongList.
+	// Add buttons, click events etc. in here. 
+	public abstract class SongFactory
+	{
+		public abstract ViewCell BuildView();
+	};
+
+	// renders a basic view cell
+	public class DefaultSongFactory : SongFactory
+	{
+		public override ViewCell BuildView()
+		{
+			// create views with bindings for displaying each property
+			Label nameLabel = new Label();
+			nameLabel.SetBinding(Label.TextProperty, "Name");
+
+			return new ViewCell
+			{
+				View = new StackLayout
+				{
+					Padding = new Thickness(0, 5),
+					Orientation = StackOrientation.Horizontal,
+					Children =
+								{
+									nameLabel,
+								}
+				},
+			};
+		}
 	};
 
 	// SongList manages presenting a list of songs. 
-	// Down the road we will extend this to include 
+	// Down the road we will extend this to include drag and drop
+	// 
+	// TODO: provide custom model to make managing straight forwards
 	public class SongList : ListView
 	{
-		public SongList(List<Song> songs)
+		public SongList(List<Song> songs, SongFactory factory)
 		{
-			this.ItemTemplate = ItemTemplate = new DataTemplate(() =>
-					{
-						// Create views with bindings for displaying each property.
-						Label nameLabel = new Label();
-						nameLabel.SetBinding(Label.TextProperty, "Name");
+			SetItemTemplateWithFactory(factory);
 
-						Label birthdayLabel = new Label();
-						birthdayLabel.SetBinding(Label.TextProperty,
-							new Binding("Birthday", BindingMode.OneWay,
-								null, null, "Born {0:d}"));
-
-						BoxView boxView = new BoxView();
-						boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
-
-						// Return an assembled ViewCell.
-						return new ViewCell
-						{
-							View = new StackLayout
-							{
-								Padding = new Thickness(0, 5),
-								Orientation = StackOrientation.Horizontal,
-								Children =
-								{
-									boxView,
-									new StackLayout
-									{
-										VerticalOptions = LayoutOptions.Center,
-										Spacing = 0,
-										Children =
-										{
-											nameLabel,
-											birthdayLabel
-										}
-										}
-								}
-							}
-						};
-					});
-
-			this.songs = songs;
-
-			// TODO: look into copy / write mechanics messing with this
+			// TODO: look into using an observable list for this
+			this.songs = new List<Song>();
 			this.ItemsSource = this.songs;
+
+			foreach (Song song in songs)
+			{
+				this.songs.Add(song);
+			}
+
+			// add tap'd handler
+			this.ItemTapped += OnItemTapped;
 		}
 
-		public List<Song> songs
+		public SongList(List<Song> songs)
+		{
+			// use the default factory
+			SetItemTemplateWithFactory(new DefaultSongFactory());
+
+			this.songs = new List<Song>();
+			this.ItemsSource = this.songs;
+
+			foreach (Song song in songs)
+			{
+				this.songs.Add(song);
+			}
+
+			// add tap'd handler
+			this.ItemTapped += OnItemTapped;
+		}
+
+		// construct an empty song list with the default song factory
+		public SongList()
+		{
+			// use the default factory
+			SetItemTemplateWithFactory(new DefaultSongFactory());
+
+			// set default song list
+			this.songs = new List<Song>();
+			this.ItemTapped += OnItemTapped;
+		}
+
+		// set the ListView's ItemTemplate to use the provided SongFactory
+		private void SetItemTemplateWithFactory(SongFactory factory)
+		{
+			this.ItemTemplate = new DataTemplate(() =>
+			{
+				return factory.BuildView();
+			});
+		}
+
+		// TODO: make this a custom list object
+		private List<Song> songs
 		{
 			// not sure that we need a "get"
 			get;
+
 			// set should clear all of the existing songs
 			set;
-
 		}
 
+		// delegate for when a song is clicked
+		public delegate void SongClickedEvent(Song song);
 
+		// called when a song is tapped on once
+		public event SongClickedEvent OnSongSelected;
+
+		// wrapper around OnItemTapped that pulls out just the selected song
+		private void OnItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			var song = (Song)e.Item;
+			this.OnSongSelected(song);
+		}
 	}
 }
