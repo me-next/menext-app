953f9eab0bee72d02d40040fbd0f8829796d9093
Daniel Centore <daniel.centore@gmail.com>
2017-03-21 21:33:14 -0400

    Fixed formatting and style

--- MeNext/net/API.cs
+++ MeNext/net/API.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using Xamarin.Forms;
 using System.Diagnostics;
 using System.Net.Http;
-using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
 
 namespace MeNext
 {
-
     public class API
     {
+        private string ServerIP { get; set; }
+        private HttpClient Client { get; set; }
+
         public API(string ip)
         {
             this.ServerIP = ip;
@@ -20,7 +21,7 @@ namespace MeNext
         ///  SayHello is a testing method that just servers to ping the server. 
         /// </summary>
         /// <returns>The server response string.</returns>
-        public async System.Threading.Tasks.Task<string> SayHello()
+        public async Task<string> SayHello()
         {
             var uri = new Uri(this.ServerIP + "/hello");
             return await FireRequest(uri);
@@ -30,7 +31,7 @@ namespace MeNext
         /// Pulls the results down from the server.
         /// </summary>
         /// <returns>The pull.</returns>
-        public async System.Threading.Tasks.Task<string> Pull(string uid, string eid, UInt64 changeID)
+        public async Task<string> Pull(string uid, string eid, UInt64 changeID)
         {
             var uri = new Uri(string.Format("/{0}/{1}/pull/{2}", uid, eid, changeID));
             return await FireRequest(uri);
@@ -41,7 +42,7 @@ namespace MeNext
         /// Try to create the party. 
         /// </summary>
         /// <returns> The party.</returns>
-        public async System.Threading.Tasks.Task<string> CreateParty(string id, string name)
+        public async Task<string> CreateParty(string id, string name)
         {
             var uri = new Uri(string.Format("/createParty/{0}/{1}", id, name));
             return await FireRequest(uri);
@@ -52,11 +53,10 @@ namespace MeNext
         /// </summary>
         /// <returns>The request.</returns>
         /// <param name="uri">URI.</param>
-        private async System.Threading.Tasks.Task<string> FireRequest(Uri uri)
+        private async Task<string> FireRequest(Uri uri)
         {
-            string result = "";
+            var result = "";
             try {
-
                 var response = await Client.GetAsync(uri);
                 if (response.IsSuccessStatusCode) {
                     result = await response.Content.ReadAsStringAsync();
@@ -67,14 +67,5 @@ namespace MeNext
 
             return result;
         }
-
-
-        private string ServerIP
-        {
-            get;
-            set;
-        }
-
-        private HttpClient Client;
     };
 }
