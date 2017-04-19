803676d28d2a67e1a4abf2d7d14de62e49f60bb3
Samuel Johnston <Samueljohnston96@gmail.com>
2017-04-19 06:23:57 -0400

    Working specific event names. However, breaks when creating two events with same name. JSON response is blank

---
+++ MeNext/Playing/Responses/CreateWithNameBadResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace MeNext
+{
+	public class CreateWithNameBadResponse
+	{
+        public CreateWithNameBadResponse()
+		{
+		}
+
+		[JsonProperty(PropertyName = "alternative")]
+		public string AltID { get; set; }
+
+		[JsonProperty(PropertyName = "error")]
+		public string Error { get; set; }
+
+	}
+}
